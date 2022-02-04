68c329d816431746aa10a1e9e4640d7240ceba48
Marcus <marcusbuffett@me.com>
2022-02-04 13:07:37 -0700

    Some more parser-grammar stuff

--- transpiler/src/parser.rs
+++ transpiler/src/parser.rs
@@ -43,7 +43,9 @@ fn parse_statement(expression: Pair<Rule>) -> Expr {
                 rhs: Box::new(rhs),
             });
         }
-        _ => unreachable!(),
+        r => {
+            panic!("Unreachable rule: {:?}", r);
+        }
     }
 }
 
@@ -51,43 +53,34 @@ fn parse_expression(expression: Pair<Rule>) -> Expr {
     let inner = expression.into_inner().next().unwrap();
     match inner.as_rule() {
         Rule::literal => return Expr::Literal(inner.as_str().parse::<u128>().unwrap()),
-        Rule::add_op => {
+        Rule::function_call => {
             let mut inners = inner.into_inner();
+            let function_name = get_identifier(inners.next().unwrap());
             let first_arg = inners.next().unwrap();
             let second_arg = inners.next().unwrap();
-            return Expr::Add(ExprAdd {
+            dbg!(&function_name);
+            return Expr::FunctionCall(ExprFunctionCall {
+                function_name: function_name.to_string(),
                 first_expr: Box::new(parse_expression(first_arg)),
                 second_expr: Box::new(parse_expression(second_arg)),
             });
         }
-        _ => unreachable!(),
+        r => {
+            panic!("Unreachable rule: {:?}", r);
+        }
     }
 }
 
-type OpCode = String;
-
-//Function to parse greater than syntax from yul into a miden opcode
-fn parse_greater_than(end: usize, syntax: &mut String) -> OpCode {
-    *syntax = syntax[end..].to_string();
-    return "GT".to_string();
-}
-
-//Function to parse greater than syntax from yul into a miden opcode
-fn parse_add(end: usize, syntax: &mut String) -> OpCode {
-    *syntax = syntax[end..].to_string();
-    return "ADD".to_string();
-}
-
-//Function to parse greater than syntax from yul into a miden opcode
-fn parse_mstore(end: usize, syntax: &mut String) -> OpCode {
-    *syntax = syntax[end..].to_string();
-    return "".to_string();
-}
-
-//Function to parse greater than syntax from yul into a miden opcode
-fn parse_if(end: usize, syntax: &mut String) -> OpCode {
-    *syntax = syntax[end..].to_string();
-    return "".to_string();
+fn get_identifier(pair: Pair<Rule>) -> String {
+    match pair.as_rule() {
+        Rule::identifier => {
+            return pair.as_str().to_string();
+            // return pair.into_inner().next().unwrap().to_string();
+        }
+        r => {
+            panic!("This was supposed to be an identifier! {:?}", r);
+        }
+    }
 }
 
 //for debugging
@@ -124,7 +117,8 @@ mod tests {
 
         let expected_ops = vec![Expr::DeclareVariable(ExprDeclareVariable {
             identifier: "x".to_string(),
-            rhs: Box::new(Expr::Add(ExprAdd {
+            rhs: Box::new(Expr::FunctionCall(ExprFunctionCall {
+                function_name: "add".to_string(),
                 first_expr: Box::new(Expr::Literal(1)),
                 second_expr: Box::new(Expr::Literal(2)),
             })),
