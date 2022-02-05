d11674926aaef89d3225c0fe83f49d7b55cc1789
machine <0xKitsune@protonmail.com>
2022-02-05 13:30:00 -0500

    updated contracts so that yul files are read in and returned as a Vec<String>

--- transpiler/src/contracts.rs
+++ transpiler/src/contracts.rs
@@ -1,24 +1,22 @@
 use std::env;
 use std::fs;
 
-fn read_yul_files() {
-    let paths = fs::read_dir("./contracts/").unwrap();
+fn read_yul_files() -> Vec<String> {
+    let mut yul_files: Vec<String> = Vec::new();
+
+    let file_path = fs::read_dir("../contracts/").unwrap();
 
     let mut parsed_files: [String; 32] = Default::default();
 
-    let mut counter:usize = 0;
-    for path in paths {
-        let mut current_file_name = path.unwrap().path().display().to_string();
-
-        println!("In file {}", current_file_name);
-
-        let contents = fs::read_to_string(current_file_name)
-        .expect("Something went wrong reading files from the contracts directory");
+    for file in file_path {
+        let mut unwrapped_file = file.unwrap().path().display().to_string();
+        let mut contents = fs::read_to_string(unwrapped_file)
+        .expect("Something went wrong readingfrom the contracts directory");
         
-        parsed_files[counter] = contents;
-        counter = counter + 1;
-        println!("With text:\n{}", parsed_files[0]);
+        contents = contents.replace('\n', "");
+        
+        yul_files.push(contents);
     }
-
+return yul_files
 }
 
