cdc5c511a5bccf872c52a7f49b020d5c6f445ed7
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2022-02-15 18:07:12 -0700

    Update bugfixes.rs

--- transpiler/tests/bugfixes.rs
+++ transpiler/tests/bugfixes.rs
@@ -4,11 +4,10 @@ use scribe::test_utilities::{run_example, MidenResult};
 #[test]
 fn test_is_zero() {
     run_example("
-        code {
         let five:u32 := 5
         let x:u32 := iszero(five)
         x
-        }", MidenResult::U32(0));
+        ", MidenResult::U32(0));
     
 }
 
