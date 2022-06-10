86c3bb7a48dfa2f485a136576f481753bab5de94
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2022-06-09 19:54:16 -0600

    Controlc/newtests (#33)
    * Easy fixes
    
    * Clippy is telling me to commit
    
    * +=
    
    * more clippy fixes
    
    * added more tests

--- transpiler/tests/milestone_2.rs
+++ transpiler/tests/milestone_2.rs
@@ -1,6 +1,6 @@
 use indoc::indoc;
+use scribe::test_utilities::compile_example;
 
-use scribe::test_utilities::{compile_example};
 
 #[test]
 fn optimization_basic_constant_replacement() {
@@ -121,5 +121,53 @@ fn optimization_let_old_vars_die() {
                 push.17
             end
         "},
+    );
+}
+
+#[test]
+fn variable_life() {
+    // Will probably have to disable some optimizations for this
+    //
+    // When a var would have been pushed to memory, it should instead be allowed to fall out of the
+    // addressable part of the stack, if it's no longer used
+    compile_example(
+        "
+    // Test mstore and mload
+    mstore(0x20, 67677686778768)
+    let b:u256 := mload(0x20)
+    let c:u256 := add(b, 1000)
+    mstore(0x20, 100)
+    let d:u256 = mload(0x20)
+    // assert the value of d
+    d
+        ",
+        todo!(),
+    );
+}
+
+#[test]
+fn test_folding() {
+    compile_example(
+        "
+    let a:u256 := add(100, 5)
+    let b:u256 := mul(a, 10)
+    let c:u256 := div(b, 5)
+    let d:u256 := sub(c, 1)
+    // Assert this folds into a single PUSH statement
+        ",
+        todo!(),
+    );
+}
+
+#[test]
+fn test_lifetime() {
+    compile_example(
+        "
+    let a:u256 := 3485488493484388458349458
+    let b:u256 := 43589348589349845838993489493
+    let c:u256 = add(a, b)
+    let d:u256 = add(a, c)
+        ",
+        todo!(),
     );
 }
